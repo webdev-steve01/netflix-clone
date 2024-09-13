ae11ee8c5786af73cbb59bb510c6ddc94f22fd44
Stephen Paul <sylvesterpaulstephen1234@gmail.com>
2024-09-13 23:09:00 +0100

    please work

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Nav from "./Nav";
 import "./globals.css";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";;
 import Main from "./signin/Main";
 import SessionProvider from "./SessionProvider";
 
@@ -20,7 +19,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = getServerSession(authOptions)
   return (
     <html lang="en">
       <link rel="shortcut icon" href="netflix-svgrepo-com.svg" type="image/x-icon" />
--- app/signin/SignInFooter.tsx
+++ app/signin/SignInFooter.tsx
@@ -38,6 +38,7 @@ function SignInFooter() {
         </select>
       </section>
       </section> */}
+      I just wan test something
     </section>
   );
 }

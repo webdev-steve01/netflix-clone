35fd64fd41d12823c620e406ea3fc5e1fb694445
Stephen Paul <sylvesterpaulstephen1234@gmail.com>
2024-09-15 18:52:22 +0100

    ui update

--- app/ReadyToWatch.tsx
+++ app/ReadyToWatch.tsx
@@ -1,34 +1,33 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 import next from "../public/right-arrow-svgrepo-com.svg";
-
 
 function ReadyToWatch() {
   return (
     <>
-       <p className="soft-text">
-          Ready to watch? enter your email to create or restart your membership
-        </p>
-        <form action="" className="form flex flex-col">
-          <section className="input flex flex-col gap-0">
-            <span className="label m-0 p-0">email address</span>
-            <input
-              className="field bg-transparent border-red border-solid border m-0"
-              title="email"
-              type="email"
-              name=""
-              id="email"
-            />
-          </section>
-          <section className="button-section">
-            <button type="button" className="button px-5 py-3 rounded-md">
-              Get started{" "}
-              <span className="next w-5 h-5">
-                <Image className="img" src={next} alt="" priority />
-              </span>
+      <p className="soft-text">
+        Ready to watch? enter your email to create or restart your membership
+      </p>
+      <form action="" className="form form-flex flex flex-col">
+        <section className="input flex flex-col gap-0">
+          <input
+            className="field bg-transparent border-red border-solid border m-0 py-2"
+            title="email"
+            type="email"
+            name=""
+            id="email"
+            placeholder="input email"
+          />
+        </section>
+        <section className="button-section">
+          <button type="button" className="button px-5 py-3 rounded-md">
+            Get started{" "}
+            <span className="next w-5 h-5">
+              <Image className="img" src={next} alt="" priority />
+            </span>
           </button>
         </section>
-        </form>
+      </form>
     </>
   );
 }

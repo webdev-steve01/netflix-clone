929a36e64cf7d74bd7c4d9a38582536f0b7ad43b
Stephen Paul <sylvesterpaulstephen1234@gmail.com>
2025-08-15 00:17:18 +0100

    fix: update not-found message to use HTML entities for apostrophes

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ function notFound() {
     <div className="flex flex-col items-center justify-center h-screen">
       <Image src={not_found} alt="not found" width={500} height={500} />
       <p className="text-white">
-        Looks like we don't have the page you're looking for
+        Looks like we don&apos;t have the page you&apos;re looking for
       </p>
     </div>
   );

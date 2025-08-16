04760bb89890e2a16ea6257681853226f7377e54
Stephen Paul <sylvesterpaulstephen1234@gmail.com>
2025-08-16 01:23:36 +0100

    fix: fixed fetch url for preview branch

--- components/Dashboard_components/DashboardNav.tsx
+++ components/Dashboard_components/DashboardNav.tsx
@@ -19,9 +19,12 @@ async function DashboardNav() {
       (item: any) => item.media_type === "movie" || item.media_type === "tv"
     );
 
-    const genreFetch = await fetch("https://fylmhouse.vercel.app/api/movies", {
-      cache: "force-cache",
-    });
+    const genreFetch = await fetch(
+      "https://netflix-clone-git-genres-page-stephen-pauls-projects.vercel.app/api/movies",
+      {
+        cache: "force-cache",
+      }
+    );
     allGenres = await genreFetch.json();
   } catch (error) {
     console.error("❌ Failed to fetch dashboard content:", error);
